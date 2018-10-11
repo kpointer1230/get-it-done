ad576d95acbe98b491e94e62c778beab628528f6
Krystal <kpointer1230@att.net>
2018-10-10 20:39:39 -0500

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:Ktp1206@localhost:8889'
 
 tasks = []
 
